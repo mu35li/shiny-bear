1156dbacb898511a82c24e4ce43c64fffd5b5641
Julian Deinert <j.deinert@gmx.net>
2015-01-11 14:45:45 +0100

    added viewport and edited routes.rb

--- shiny-bear/config/routes.rb
+++ shiny-bear/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :stocks
-  patch 'stocks' => 'stocks#new'
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
